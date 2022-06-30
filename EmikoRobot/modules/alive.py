eabe3dfd81036d53d4c93e6053ad0eb24b6d681e
DazBot <95157942+DazRepo@users.noreply.github.com>
2022-06-30 15:42:52 +0700

    Update alive.py

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -9,7 +9,7 @@ from EmikoRobot.events import register
 from EmikoRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/2c13e570944404a6c0d86.jpg"
+PHOTO = "https://telegra.ph/file/d0619994d9ed62070ef76.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
