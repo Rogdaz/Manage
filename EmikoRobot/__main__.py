df52fd08379359b7db878b3d0bdd2dae5360ff15
GTB-Dazii <95157942+DazRepo@users.noreply.github.com>
2022-08-02 11:59:21 +0700

    Update __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -93,7 +93,7 @@ PM_START_TEXT = """
 × `{}` *Uꜱᴇʀ, Aᴄʀᴏꜱꜱ* `{}` *Cʜᴀᴛꜱ.*
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 ◕ *klik help & cmd untuk melihat commands.*
-◕ *Pᴏᴡᴇʀᴇᴅ* [☕](https://telegra.ph/file/be460a5c253d3539059fe.jpg) *Bʏ: @about_db!*
+◕ *Pᴏᴡᴇʀᴇᴅ* [☕](https://telegra.ph/file/1afa11b71228ef67aefc5.jpg) *Bʏ: @about_db!*
 """
 
 buttons = [
